8a76e128d3514edbb71d471e6eb96823af676655
Erick-Rangel <rebel863@hotmail.com>
2021-08-27 23:47:36 -0500

    prueba1

---
+++ homework/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Card from "./components/Card.jsx";
+import Cards from "./components/Cards.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import styles from "./App.module.css";
+import fetchCity from "./services/fetchCity.js";
+
+function App() {
+  const [data, setData] = React.useState([]);
+  function onSearch(ciudad) {
+    if (data?.length > 2) {
+      alert("No puedes agregar más ciudades.");
+    } else {
+
+      fetchCity(ciudad, setData);
+    }
+  } 
+
+  function handleOnClose(id) {
+    setData((prevData) => {
+     return prevData.filter((city) => city.id !== id);
+    });
+  }
+
+  return (
+    <div>
+      <div className={styles.app}>
+        <div className={styles.bkg} />
+        <div className={styles.container}>
+          <div>
+            <SearchBar onSearch={onSearch} />
+          </div>
+          <div className={styles.citiesContainer}>
+            {data.length > 0 && (
+              <Card
+                primary
+                max={data[data?.length - 1].max}
+                min={data[data?.length - 1].min}
+                name={data[data?.length - 1].name}
+                img={data[data?.length - 1].img}
+              />
+            )}
+            <Cards cities={data} onClose={handleOnClose} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ homework/src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+import { IoAdd } from "react-icons/io5";
+
+export default function SearchBar({ onSearch }) {
+  // acá va tu código
+  const [search, setSearch] =React.useState("")
+  const handleOnSearch = () => {
+   onSearch(search)
+   setSearch("")
+  };
+
+  return (
+    <div className={styles.searchBar}>
+      <input
+        className={styles.input}
+        id="searchInput"
+        placeholder="Agregar una cuidad..."
+        autoComplete="off"
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
+        onKeyPress={(e)=> {
+          if(e.key === "Enter")handleOnSearch();
+      }}
+      />
+      <button className={styles.button} onClick={handleOnSearch}>
+        <IoAdd />
+      </button>
+    </div>
+  );
+}
